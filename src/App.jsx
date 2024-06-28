47b6fb83f176a3412c6037925318cbe159737e1f
Avnish Kumar <avnish-kumar@prod.hclpnp.com>
2024-06-28 07:56:18 +0530

    fist commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components"
+
+const Button= styled.button`
+  background-color:#4e81b3;
+  color: #0C1844;
+  padding: 10px;
+  font-weight: bold;
+`;
+
+function App() {
+
+
+  return (
+    <>
+      <Button>This is Styled Button</Button>
+    </>
+  )
+}
+
+export default App
